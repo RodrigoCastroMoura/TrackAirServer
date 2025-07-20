378cc8eb0e4d4f03e4d7974b25763943a2224ce2
rodrigocastromo <37327445-rodrigocastromo@users.noreply.replit.com>
2025-07-20 16:08:11 +0000

    Initial commit

---
+++ attached_assets/Mensagem_1753025652739.cs
@@ -0,0 +1,56 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using RastreioFacil.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RastreioFacil.Domain.Entities
+{
+    public class Mensagem
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; private set; }
+
+        public string cpf { get; private set; }
+
+        public string IMEI { get; private set; }
+
+        public int  id_tipo_Mensagem { get; private set; }
+
+        public string ds_mensagem { get; private set; }
+
+        public string ds_mensagemHtml { get; private set; }
+
+        public DateTime? dt_mensagem { get; private set; }
+
+        public bool? fl_lida { get; private set; }
+
+        public virtual TipoMensagem tipoMensagem { get; set; }
+
+
+        private Mensagem(MensagemDto mensagem)
+        {
+            if (mensagem != null) { 
+
+                this._id = mensagem._id;
+                this.cpf = mensagem.cpf;
+                this.IMEI = mensagem.IMEI;
+                this.id_tipo_Mensagem = mensagem.id_tipo_Mensagem;
+                this.ds_mensagem = mensagem.ds_mensagem;
+                this.dt_mensagem = mensagem.dt_mensagem;
+                this.fl_lida = mensagem.fl_lida;
+                this.ds_mensagemHtml = mensagem.ds_mensagemHtml;
+            }
+        }
+
+       public static Mensagem RetornoMensagem(MensagemDto mensagemDto)
+       {
+           return new Mensagem(mensagemDto);
+       }           
+       
+    }
+}
+
